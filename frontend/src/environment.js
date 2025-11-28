db045b793f2c32f8a1cefd5895f0ac7e45701cb3
akarshan gupta <akarshang31@gmail.com>
2025-11-29 03:24:34 +0530

    hosted on render

---
+++ frontend/src/environment.js
@@ -0,0 +1,3 @@
+let IS_PROD = true;
+const server = IS_PROD ? "https://video-conferencing-backend-6sbq.onrender.com" : "http://localhost:8000";
+export default server;
--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -13,7 +13,7 @@ import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
 import ChatIcon from "@mui/icons-material/Chat";
 import { useNavigate } from "react-router-dom";
 
-const server_url = "https://video-conferencing-psi.vercel.app";
+const server_url = server;
 let connections = {};
 
 const peerConfigConnections = {
