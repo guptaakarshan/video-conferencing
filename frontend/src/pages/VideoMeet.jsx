f27501e98898622f9afe0fc009afddd8708684f6
akarshan gupta <akarshang31@gmail.com>
2025-11-29 03:30:37 +0530

    imported server

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -12,6 +12,7 @@ import ScreenShareIcon from "@mui/icons-material/ScreenShare";
 import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
 import ChatIcon from "@mui/icons-material/Chat";
 import { useNavigate } from "react-router-dom";
+import server from '../environment';
 
 const server_url = server;
 let connections = {};
