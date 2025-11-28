ad17b4156188e626c19769c128c46e1fd302b52a
akarshan gupta <akarshang31@gmail.com>
2025-11-28 23:27:06 +0530

    🖊️ | Added Domain

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -5,7 +5,7 @@ import httpStatus from "http-status";
 
 export const AuthContext = createContext({});
 const client = axios.create({
-  baseURL: "http://localhost:8000/api/v1/users",
+  baseURL: "http://video-conferencing-psi.vercel.app/api/v1/users",
 });
 
 export const AuthProvider = ({ children }) => {
